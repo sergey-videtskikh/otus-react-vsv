4024b7314fd79171d6a4fb69897f195ce7dc65d0
Sergey.Videtskikh <sergey.videtskikh@russianpost.ru>
2022-03-25 17:37:31 +0300

    install webpack

---
+++ lesson2-hw1/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Calculator',
+            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+}
+
